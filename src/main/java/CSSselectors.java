8d580099a458daaf4b6d9861ab1c9a659f281351
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-09-14 11:58:24 -0700

    added code examples to the cssselector file

--- src/main/java/CSSselectors.java
+++ src/main/java/CSSselectors.java
@@ -1,5 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.DriverManager;
+
 public class CSSselectors {
     public static void main(String[] args) {
-
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com");
+        //driver.get("http://localhost/sdetpractice.com");
+        driver.findElement(By.cssSelector("img[alt='Picture of Build your own computer']")).click();
     }
 }
