55099d3bc5c3124a9150a25daaf546d0a403c034
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-13 07:42:49 -0800

    added more code examples

--- src/main/java/XpathLocators.java
+++ src/main/java/XpathLocators.java
@@ -4,14 +4,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class XpathLocators {
     public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","/Users/thebeast/Documents/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+
 
         driver.get("https://rahulshettyacademy.com/locatorspractice");
         driver.findElement(By.linkText("Forgot your password?")).click();
         driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("Yousufzai");
-        Thread.sleep(5000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
         //traversing from parent to child element using xpath
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("912094345");
     }
 }
