4d1e57da5c14f68f2f3882580ccfa5d96cd4d1c3
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-14 07:16:54 -0800

    added handlingbrowser class and some code examples in it

---
+++ src/main/java/HandlingBrowser.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandlingBrowser {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://google.com");
+
+        driver.navigate().to("https://rahulshettyacademy.com");
+
+        driver.navigate().back();
+
+        driver.navigate().forward();
+    }
+}
--- src/main/java/MoreXpath.java
+++ src/main/java/MoreXpath.java
@@ -1,2 +1,9 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MoreXpath {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/automationpractice");
+    }
 }
