3dc42727e8efcbf4cca675a88a9f5963c204e131
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-14 06:37:08 -0800

    added getpassword function to extract the password from success message on reset link

--- src/main/java/Locators2.java
+++ src/main/java/Locators2.java
@@ -10,12 +10,14 @@ public class Locators2 {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-
-        driver.get("https://rahulshettyacademy.com/locatorspractice");
+        String url="https://rahulshettyacademy.com/locatorspractice";
+       String dynamicPassword= getPassword(driver,url);
+        driver.get(url);
+        Thread.sleep(2000);
 
         //login to the site
         driver.findElement(By.id("inputUsername")).sendKeys(name);
-        driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys(dynamicPassword);
         driver.findElement(By.id("chkboxOne")).click();
 
         //finding element using the xpath regular expression
@@ -28,5 +30,21 @@ public class Locators2 {
         String welcomeMessage= driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText();
         System.out.println(welcomeMessage);
         Assert.assertEquals(welcomeMessage,"Hello "+name+",");
+        driver.findElement(By.xpath("//button[@class='logout-btn']")).click();
+        Thread.sleep(2000);
+        driver.close();
+    }
+    public static String getPassword(WebDriver driver,String url) throws InterruptedException {
+        driver.get(url);
+//        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[normalize-space()='Forgot your password?']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        String successResetMessage = driver.findElement(By.cssSelector("form p")).getText();
+        String password[] = successResetMessage.split("'");
+        String passwordText = password[1].split("'")[0];
+        System.out.println(passwordText);
+        return passwordText;
+
     }
 }
