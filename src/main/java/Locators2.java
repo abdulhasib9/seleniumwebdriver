6b9d85ec81a758bb4294f8b04f4bd173dff86d54
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-14 05:43:38 -0800

    added more code examples for confirming the welcome message

--- src/main/java/Locators2.java
+++ src/main/java/Locators2.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Locators2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        String name= "rahul";
         System.setProperty("webdriver.chrome.driver","/Users/thebeast/Documents/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -12,10 +14,19 @@ public class Locators2 {
         driver.get("https://rahulshettyacademy.com/locatorspractice");
 
         //login to the site
+        driver.findElement(By.id("inputUsername")).sendKeys(name);
         driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
         driver.findElement(By.id("chkboxOne")).click();
 
         //finding element using the xpath regular expression
         driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+        Thread.sleep(1000);
+        String successMessage = driver.findElement(By.tagName("p")).getText();
+       // System.out.println(successMessage);
+        Assert.assertEquals(successMessage,"You are successfully logged in.");
+
+        String welcomeMessage= driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText();
+        System.out.println(welcomeMessage);
+        Assert.assertEquals(welcomeMessage,"Hello "+name+",");
     }
 }
