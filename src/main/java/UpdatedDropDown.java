ff0eae071c6280cd19de918aad3b4578cf8fb0ff
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-15 06:59:30 -0800

    added more code examples to work with updated drop down list

--- src/main/java/UpdatedDropDown.java
+++ src/main/java/UpdatedDropDown.java
@@ -1,2 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class UpdatedDropDown {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        //divpaxinfo
+        driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(2000L);
+        //adding four adults
+        for (int i =0;i<=3;i++)  {
+            driver.findElement(By.id("hrefIncAdt")).click();
+        }
+        //driver.findElement(By.id("hrefIncAdt")).click();
+        //Thread.sleep(2000);
+        driver.findElement(By.id("btnclosepaxoption")).click();
+
+        Thread.sleep(7000);
+        driver.close();
+    }
 }
