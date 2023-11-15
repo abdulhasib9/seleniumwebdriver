338798f4c121ff7d517e90e30de4b4cf4ae0a402
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-15 06:40:35 -0800

    added code example for select dropdown

--- src/main/java/HandlingDropDowns.java
+++ src/main/java/HandlingDropDowns.java
@@ -1,2 +1,23 @@
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class HandlingDropDowns {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        //select dropdown with select tag
+        WebElement staticDropDown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+        Select dropDown = new Select(staticDropDown);
+        dropDown.selectByIndex(3);
+        String selectedItem = dropDown.getFirstSelectedOption().getText();
+        System.out.println(selectedItem);
+
+
+
+    }
 }
