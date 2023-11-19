2c2dedb3f261689fb82d14515660dee3adea9d5f
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-18 23:42:40 -0800

    added more code examples

--- src/main/java/DynamicDropdowns.java
+++ src/main/java/DynamicDropdowns.java
@@ -1,2 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DynamicDropdowns {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+    }
 }
