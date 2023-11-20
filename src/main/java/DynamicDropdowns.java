ac9156947bde087da1beab8e4232c69d54580375
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-19 23:21:10 -0800

    added more code examples for handling dynamic dropdowns

--- src/main/java/DynamicDropdowns.java
+++ src/main/java/DynamicDropdowns.java
@@ -3,10 +3,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DynamicDropdowns {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
         driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[@value='MAA']")).click();
+
+        Thread.sleep(7000);
+        driver.close();
     }
 }
