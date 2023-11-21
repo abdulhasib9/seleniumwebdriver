f7ec91b8349c4fbad0efc5632e82ed3bb80b82da
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-20 21:16:37 -0800

    added updated way to handling dropdown without index

--- src/main/java/DynamicDropdowns.java
+++ src/main/java/DynamicDropdowns.java
@@ -10,8 +10,10 @@ public class DynamicDropdowns {
         driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
         driver.findElement(By.xpath("//a[@value='BLR']")).click();
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//a[@value='MAA']")).click();
-
+       // driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+        //handling dynamic dropdowns by not using index
+        //child to parent travers
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='AAA']")).click();
         Thread.sleep(7000);
         driver.close();
     }
