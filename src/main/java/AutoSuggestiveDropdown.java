6a03eeeb7ca22203cab7dcc1a109fd509c2eda1f
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-20 21:27:16 -0800

    added code example for auto suggestion in search bar

--- src/main/java/AutoSuggestiveDropdown.java
+++ src/main/java/AutoSuggestiveDropdown.java
@@ -1,3 +1,23 @@
-public class AutoSuggestiveDropdown {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
+public class AutoSuggestiveDropdown {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("autosuggest")).sendKeys("ind");
+        Thread.sleep(2000);
+        List<WebElement> items =driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+        for(WebElement item : items){
+            if(item.getText().equalsIgnoreCase("India")){
+                item.click();
+                break;
+            }
+        }
+    }
 }
