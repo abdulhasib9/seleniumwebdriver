a9c189ddf588cad2c4569f7e7ec1bde05df4f6f1
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-21 11:26:02 -0800

    added code example for handling checkbox

--- src/main/java/HandlingCheckBoxes.java
+++ src/main/java/HandlingCheckBoxes.java
@@ -1,2 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.List;
+
 public class HandlingCheckBoxes {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        Assert.assertFalse( driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+        driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        Thread.sleep(3000);
+        //selecting all checkbox at once
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector("input[type='checkbox']"));
+        for (WebElement item:checkBoxes
+             ) {
+            item.click();
+        }
+
+        Thread.sleep(5000);
+        driver.close();
+
+    }
 }
